3f45695d5cd9cdadf7a7003e41747fb0f2304b7e
OlegMartynov <olegmms@gmail.com>
2024-03-20 17:51:24 +0300

    Added Gradle Versions Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,13 @@ plugins {
     id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.51.0"
+    id("org.springframework.boot") version "3.2.3"
+    application
 }
+apply(plugin = "io.spring.dependency-management")
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "0.5-pre-release"
 
 repositories {
     mavenCentral()
@@ -15,8 +18,16 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+application {
+    mainClass = "org.example.Main"
+}
+springBoot {
+    mainClass = "org.example.Main"
 }
